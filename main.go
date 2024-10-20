7bafa50a0e2f1d7e6f25598b2b46c6ce986457ff
Andy Fong <andy.fong@solo.io>
2024-10-19 22:05:24 -0400

    working

--- main.go
+++ main.go
@@ -16,82 +16,115 @@ const (
 )
 
 type ChangeLogEntry struct {
-	CommitHash         []byte
-	PR                 string
+	CommitHashes       []string
+	PR                 []string
 	Category           string
 	Area               string
-	Summary            string
+	Summary            []string
 	Description        string
 	ProcessChangeLines bool
 }
 
 type ChangeLogs struct {
-	logs              []*ChangeLogEntry
-	hexRegexp         *regexp.Regexp
-	categoryRegexp    *regexp.Regexp
-	prRegexp          *regexp.Regexp
-	currentCommitHash string
-	currentCategory   string
-	currentEntry      *ChangeLogEntry
+	logs             []*ChangeLogEntry
+	hexRegexp        *regexp.Regexp
+	categoryRegexp   *regexp.Regexp
+	prRegexp         *regexp.Regexp
+	lastCommitHash   string
+	currentCategory  string
+	currentEntry     *ChangeLogEntry
+	commitSummaryMap map[string]string
 }
 
-func (c *ChangeLogs) ProcessContent(entry *ChangeLogEntry, line string) {
+func (c *ChangeLogs) addCurrentEntry() {
+	if c.currentEntry != nil && len(c.currentEntry.Category) != 0 {
+		for _, commitHash := range c.currentEntry.CommitHashes {
+			summary := c.commitSummaryMap[commitHash]
+			c.currentEntry.Summary = append(c.currentEntry.Summary, summary)
+			pr := c.prRegexp.FindString(summary)
+			if len(pr) > 4 {
+				c.currentEntry.PR = append(c.currentEntry.PR, pr[2:len(pr)-1])
+			} else {
+				//				fmt.Printf("warning: cannot find PR from summary: %s\n", c.currentEntry.Summary)
+			}
+		}
+		c.logs = append(c.logs, c.currentEntry)
+	}
+}
+
+func (c *ChangeLogs) createNewCurrentEntry() {
+	c.currentEntry = &ChangeLogEntry{
+		Category: c.currentCategory,
+	}
+	c.currentEntry.CommitHashes = append(c.currentEntry.CommitHashes, c.lastCommitHash)
+}
+
+func (c *ChangeLogs) ProcessContent(line string) {
 	line = strings.TrimLeft(line, "\t")
 	//	fmt.Printf("processing: %s\n", line)
+
+	if len(line) == 0 {
+		return
+	}
 	if line == c.categoryRegexp.FindString(line) {
 		c.currentCategory = strings.TrimRight(line, ":")
-		fmt.Printf("found category: %s\n", c.currentCategory)
+		//		fmt.Printf("found category: %s\n", c.currentCategory)
 		return
 	}
 
+	if strings.HasPrefix(line, "- ") {
+		c.addCurrentEntry()
+		c.createNewCurrentEntry()
+	}
+
+	if c.currentEntry == nil {
+		return
+	}
+
+	commitHashAlreadyExists := false
+	for _, commitHash := range c.currentEntry.CommitHashes {
+		if commitHash == c.lastCommitHash {
+			commitHashAlreadyExists = true
+		}
+	}
+	if !commitHashAlreadyExists {
+		c.currentEntry.CommitHashes = append(c.currentEntry.CommitHashes, c.lastCommitHash)
+	}
 	line = strings.TrimLeft(line, "- ")
 
-	if entry.ProcessChangeLines {
-		entry.Description += line + " "
+	if c.currentEntry.ProcessChangeLines {
+		c.currentEntry.Description += line + " "
 		return
 	}
 
 	if strings.HasPrefix(line, areaField) {
-		entry.Area = line[len(areaField):]
+		c.currentEntry.Area = line[len(areaField):]
+		return
 	}
 
 	if strings.HasPrefix(line, changeField) {
-		entry.ProcessChangeLines = true
+		c.currentEntry.ProcessChangeLines = true
+		return
 	}
 }
 
 func (c *ChangeLogs) ProcessGitBlameOutput(line string) {
-	parts := strings.SplitN(line, " ", 2)
-	if parts[0] == c.hexRegexp.FindString(parts[0]) && parts[0] != c.currentCommitHash {
-		if c.currentEntry != nil && len(c.currentEntry.Category) != 0 {
-			fmt.Println(c.currentCommitHash)
-			c.logs = append(c.logs, c.currentEntry)
-		}
-		c.currentCommitHash = parts[0]
-		c.currentEntry = &ChangeLogEntry{
-			CommitHash: []byte(c.currentCommitHash[0:11]),
-			Category:   c.currentCategory,
-		}
-	}
-
 	if strings.HasPrefix(line, "\t") {
-		c.ProcessContent(c.currentEntry, line)
-	}
-
-	if len(c.currentCategory) == 0 || c.currentEntry == nil {
+		c.ProcessContent(line)
 		return
+	} else {
+		parts := strings.SplitN(line, " ", 2)
+		if parts[0] == c.hexRegexp.FindString(parts[0]) {
+			c.lastCommitHash = parts[0]
+			return
+		}
 	}
 
 	if strings.HasPrefix(line, summaryField) {
-		c.currentEntry.Summary = line[len(summaryField):]
-		pr := c.prRegexp.FindString(c.currentEntry.Summary)
-		if len(pr) > 4 {
-			c.currentEntry.PR = pr[2 : len(pr)-1]
-		} else {
-			fmt.Printf("warning: cannot find PR from summary: %s\n", c.currentEntry.Summary)
-		}
+		summary := line[len(summaryField):]
+		// fmt.Printf("%s: %s\n", c.lastCommitHash, summary)
+		c.commitSummaryMap[c.lastCommitHash] = summary
 	}
-
 }
 
 func main() {
@@ -123,27 +156,30 @@ func main() {
 
 	scanner := bufio.NewScanner(stdout)
 	changeLogs := &ChangeLogs{
-		categoryRegexp: regexp.MustCompile(`^[a-z]+:$`),
-		hexRegexp:      regexp.MustCompile(`[0-9a-f]+`),
-		prRegexp:       regexp.MustCompile(`\(#[0-9]+\)`),
+		categoryRegexp:   regexp.MustCompile(`^[a-z_]+:$`),
+		hexRegexp:        regexp.MustCompile(`[0-9a-f]+`),
+		prRegexp:         regexp.MustCompile(`\(#[0-9]+\)`),
+		commitSummaryMap: make(map[string]string),
 	}
 
 	for scanner.Scan() {
 		changeLogs.ProcessGitBlameOutput(scanner.Text())
 	}
+	// Takes care of the last outstanding entry
+	changeLogs.addCurrentEntry()
 
 	if err := cmd.Wait(); err != nil {
 		panic(err)
 	}
 
 	for _, entry := range changeLogs.logs {
-		fmt.Printf("commit: %s\n", entry.CommitHash)
-		fmt.Printf("pr: %s\n", entry.PR)
+		fmt.Printf("commit: %v\n", entry.CommitHashes)
+		fmt.Printf("pr: %v\n", entry.PR)
 		fmt.Printf("category: %s\n", entry.Category)
 		fmt.Printf("area: %s\n", entry.Area)
-		fmt.Printf("summary: %s\n", entry.Summary)
+		fmt.Printf("summary: %v\n", entry.Summary)
 		fmt.Printf("description:\n%s\n", entry.Description)
-		fmt.Printf("detected Change: %v\n", entry.ProcessChangeLines)
+		//		fmt.Printf("detected Change: %v\n", entry.ProcessChangeLines)
 		fmt.Println("----------------------------------------------------------------")
 	}
 }
